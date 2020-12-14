1b01de3fba9d240e8ff53dc14bf468686f4fa599
cll2020 <1246873024@qq.com>
2020-12-14 20:59:45 +0800

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -3,7 +3,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/11'
 CELERY_TIMEZONE = 'Asia/Shanghai'
 CELERYBEAT_SCHEDULE = {
         'task_cron_job_10_sec': {
-            'task': 'proj.tasks.celery_test_print',
-            'schedule': 10,
+            'task': 'celery_test_print',
+            'schedule': 1,
         },
 }
